beed442946e5f5057b8ef97241535dd7b7203e09
Chessboxer <mgregor81@gmail.com>
2020-05-08 13:56:19 -0700

    Add project files.

---
+++ NetworkingPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetworkingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
